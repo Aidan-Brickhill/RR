6a7e4b64bdbbba36ae9a5a9068370b029b08a95e
Aidan Brickhill <2427688@students.wits.ac.za>
2024-05-09 17:03:22 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from gymnasium.envs.registration import register
-
-register(
-     id="RR/GridWorld-v0",
-     entry_point="RR.envs:handover",
-     max_episode_steps=300,
-)
-
 from setuptools import setup
 
 setup(
