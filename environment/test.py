b875a735c1b157babbfbf57f31e4299b7a5d7485
Aidan-Brickhill <2427688@students.wits.ac.za>
2024-06-07 13:38:51 +0200

    testing working function

--- environment/test.py
+++ environment/test.py
@@ -1,11 +1,11 @@
 from stable_baselines3 import PPO
 from handover_env import HandoverEnv
 
-model_path = "/home/aidan/00_Ubuntu_Honours/RR/RR/environment/models/PPO/model_2.zip"
-model_path = "/home/aidan/Ubuntu/RR/RR/environment/models/PPO/model_3.zip"
+model_path = "/home/aidan/00_Ubuntu_Honours/RR/RR/environment/models/PPO/model_4.zip"
+# model_path = "/home/aidan/Ubuntu/RR/RR/environment/models/PPO/model_3.zip"
 
 
-env = HandoverEnv(render_mode="human", tasks_to_complete = ["panda_giver_fetch", "object_lift", "object_move","panda_reciever_wait"], max_episode_steps=300)
+env = HandoverEnv(render_mode="human", tasks_to_complete = ["panda_giver_fetch", "object_lift", "object_move","panda_reciever_wait"], max_episode_steps=400)
 env.reset()
 
 model = PPO.load(model_path, env=env)
