4314fb1272612e16e451bc0fb9e03db60b778d88
Aidan-Brickhill <2427688@students.wits.ac.za>
2024-06-14 18:39:33 +0200

    variation 1

--- environment/handover_env.py
+++ environment/handover_env.py
@@ -314,18 +314,36 @@ class HandoverEnv(gym.Env, EzPickle):
             distance_giver_object = np.linalg.norm(achieved_goal["panda_giver_fetch"] - achieved_goal["object_move"])
 
             # give a reward based on distance and scale it based on whether the robot fingers are touching the object
+            # VARIATION 0 ==========
+            # if good_collisons.count("inside_giver_robot_rightfinger_object_col") == 1 and good_collisons.count("inside_giver_robot_leftfinger_object_col") == 1:
+            #     # both fingers (inside)
+            #     combined_reward += 5 * (1-np.tanh(distance_giver_object))
+            # elif good_collisons.count("inside_giver_robot_rightfinger_object_col") == 1 or good_collisons.count("inside_giver_robot_leftfinger_object_col") == 1:
+            #     # 1 finger (inside)
+            #     combined_reward += 3 * (1-np.tanh(distance_giver_object))
+            # elif good_collisons.count("giver_robot_finger_object_col") == 2:
+            #     # both fingers
+            #     combined_reward += 1.5 * (1-np.tanh(distance_giver_object))
+            # elif good_collisons.count("giver_robot_finger_object_col") == 1:
+            #     # 1 finger
+            #     combined_reward += 0.75 * (1-np.tanh(distance_giver_object))
+            # else:
+            #     # no finger
+            #     combined_reward += 0.25 * (1-np.tanh(distance_giver_object))
+
+            # VARIATION 1 ==========
             if good_collisons.count("inside_giver_robot_rightfinger_object_col") == 1 and good_collisons.count("inside_giver_robot_leftfinger_object_col") == 1:
                 # both fingers (inside)
-                combined_reward += 5 * (1-np.tanh(distance_giver_object))
+                combined_reward += 5 + 5 * (1-np.tanh(distance_giver_object))
             elif good_collisons.count("inside_giver_robot_rightfinger_object_col") == 1 or good_collisons.count("inside_giver_robot_leftfinger_object_col") == 1:
                 # 1 finger (inside)
-                combined_reward += 3 * (1-np.tanh(distance_giver_object))
+                combined_reward += 3 + 3 * (1-np.tanh(distance_giver_object))
             elif good_collisons.count("giver_robot_finger_object_col") == 2:
                 # both fingers
-                combined_reward += 1.5 * (1-np.tanh(distance_giver_object))
+                combined_reward += 2 + 2 * (1-np.tanh(distance_giver_object))
             elif good_collisons.count("giver_robot_finger_object_col") == 1:
                 # 1 finger
-                combined_reward += 0.75 * (1-np.tanh(distance_giver_object))
+                combined_reward += 1 + (1-np.tanh(distance_giver_object))
             else:
                 # no finger
                 combined_reward += 0.25 * (1-np.tanh(distance_giver_object))
@@ -348,7 +366,7 @@ class HandoverEnv(gym.Env, EzPickle):
                 distance_object = np.linalg.norm(achieved_goal["object_move"] - desired_goal["object_move"])
 
                 # provide relative reward based on the distance
-                combined_reward += 10 * (1-np.tanh(distance_object))
+                combined_reward += 10 + 10 * (1-np.tanh(distance_object))
                     
                 # if the object is in the goal position 
                 if distance_object < OBJECT_MOVE_THRESH:
@@ -368,7 +386,11 @@ class HandoverEnv(gym.Env, EzPickle):
                 # if the height increases
                 if "panda_giver_fetch" in self.episode_task_completions:
                     if achieved_goal["object_lift"][0] > max_object_height + 0.001:
-                        combined_reward += 8 
+                        # VARIATION 0=========
+                        # combined_reward += 8 
+                        # VARIATION 1=========
+                        combined_reward += 12 
+
                 
             # get the distance between the end effector and the goal positon
             distance_reciever = np.linalg.norm(achieved_goal["panda_reciever_wait"] - desired_goal["panda_reciever_wait"])
