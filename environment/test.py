397f33dbd3f74fddf7ab792c63836f20cbac7f76
Aidan-Brickhill <2427688@students.wits.ac.za>
2024-07-29 16:06:43 +0200

    ye

--- environment/test.py
+++ environment/test.py
@@ -1,7 +1,7 @@
 from stable_baselines3 import PPO
 from handover_env import HandoverEnv
 
-model_path = "RR/environment/models/PPO/pickup"
+model_path = "RR/environment/models/PPO/handover"
 
 
 # env = HandoverEnv(render_mode="human", tasks_to_complete = ["panda_giver_fetch", "object_lift",,"panda_reciever_wait"], max_episode_steps=300)
@@ -15,7 +15,7 @@ for ep in range(episodes):
     obs, info = env.reset()
     terminated = False
 
-    while not terminated:
+    while not terminated: 
         action, _states = model.predict(obs)
         obs, reward, terminated, truncated, info = env.step(action)
 
