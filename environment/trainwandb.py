c70f06ae83f668f9029672c562002aa2998ebd3f
Aidan-Brickhill <2427688@students.wits.ac.za>
2024-08-02 12:43:08 +0200

    fixe dtask error

--- environment/trainwandb.py
+++ environment/trainwandb.py
@@ -25,7 +25,7 @@ def make_env():
     # handover
     # return Monitor(HandoverEnv(render_mode="rgb_array",tasks_to_complete = ["panda_reciever_wait", "object_move_p2", "panda_reciever_fetch","panda_reciever_place","panda_giver_retreat"], max_episode_steps = 300))
     # BOTH
-    return Monitor(HandoverEnv(render_mode="rgb_array",tasks_to_complete = ["panda_giver_fetch", "object_lift", "panda_reciever_wait", "object_move_p2", "panda_reciever_fetch","panda_reciever_place","panda_giver_retreat"], max_episode_steps = 400))
+    return Monitor(HandoverEnv(render_mode="rgb_array",tasks_to_complete = ["object_move_place", "object_move_handover", "panda_giver_retreat", "panda_giver_fetch","panda_reciever_wait","panda_reciever_fetch"], max_episode_steps = 400))
 
 env= DummyVecEnv([make_env] * 4)
 
